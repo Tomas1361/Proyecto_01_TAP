b0336a93f797a033d5b60ccdcfc3de3c9e895434
Takosg805 <mg4142642@gmail.com>
2026-03-19 01:37:32 -0600

    Cambios

--- src/proyecto101/Ventana.java
+++ src/proyecto101/Ventana.java
@@ -17,7 +17,8 @@ public class Ventana extends JFrame {
         new ImageIcon(getClass().getResource("Rojo.png")),
         new ImageIcon(getClass().getResource("Borde_Negro.png")),
         new ImageIcon(getClass().getResource("Borde_Azul.png")),
-        new ImageIcon(getClass().getResource("Borde_Rojo.png")),};
+        new ImageIcon(getClass().getResource("Borde_Rojo.png"))
+    };
     private ImageIcon airForce[] = {
         new ImageIcon(getClass().getResource("AirForceBlanco.png")),
         new ImageIcon(getClass().getResource("AirForceNegro.png")),
@@ -62,12 +63,52 @@ public class Ventana extends JFrame {
         new ImageIcon(getClass().getResource("Borde_Negro.png")),
         new ImageIcon(getClass().getResource("Borde_Azul.png"))
     };
+    private ImageIcon stars[] = {
+        new ImageIcon(getClass().getResource("StarsNegros.png")),
+        new ImageIcon(getClass().getResource("StarsAmarillos.png"))
+    };
+    private ImageIcon coloresStars[] = {
+        new ImageIcon(getClass().getResource("Blanco_Negro.png")),
+        new ImageIcon(getClass().getResource("Amarillo_Blanco_Azul.png")),
+        new ImageIcon(getClass().getResource("Borde_Blanco_Negro.png")),
+        new ImageIcon(getClass().getResource("Borde_Amarillo_Blanco_Azul.png"))
+    };
+    private ImageIcon under[] = {
+        new ImageIcon(getClass().getResource("Under_Negro.png")),
+        new ImageIcon(getClass().getResource("Under_Gris.png")),
+        new ImageIcon(getClass().getResource("Under_Azul.png")),
+        new ImageIcon(getClass().getResource("Under_Rojo.png")),
+        new ImageIcon(getClass().getResource("Under_Verde.png"))
+    };
+    private ImageIcon coloresUnder[] = {
+        new ImageIcon(getClass().getResource("Negro.png")),
+        new ImageIcon(getClass().getResource("Gris.png")),
+        new ImageIcon(getClass().getResource("Azul.png")),
+        new ImageIcon(getClass().getResource("Rojo.png")),
+        new ImageIcon(getClass().getResource("Verde.png")),
+        new ImageIcon(getClass().getResource("Borde_Negro.png")),
+        new ImageIcon(getClass().getResource("Borde_Gris.png")),
+        new ImageIcon(getClass().getResource("Borde_Azul.png")),
+        new ImageIcon(getClass().getResource("Borde_Rojo.png")),
+        new ImageIcon(getClass().getResource("Borde_Verde.png"))
+    };
 
     // Atributos de los productos
     private JPanel producto1 = crearProducto("Converse", 120, "Tenis Converse Chuck Taylor All Star Negros en Bota de Lona Unisex", converse, coloresConverse);
     private JPanel producto2 = crearProducto("Air Force 1", 80, "Tenis Unisex Nike Air Force 1 '07 Cw2288-111", airForce, coloresAirForce);
     private JPanel producto3 = crearProducto("Air Jordan 3", 200, "Nike WMNS Air Jordan 3 RETRO", airJordan, coloresJordan);
     private JPanel producto4 = crearProducto("Campus 00s", 200, "Tenis Adidas Campus 00's Clasico Original", campus, coloresCampus);
+    private JPanel producto5 = crearProducto("Stars", 200, "TENIS HOMBRE STARS CU413", stars, coloresStars);
+    private JPanel producto6 = crearProducto("UA Rogue 4", 200, "Tenis deportivos Under Armour ligeros con suela blanca texturizada.", under, coloresUnder);
+
+    private JPanel listaProductos[] = {producto1, producto2, producto3, producto4, producto5, producto6};
+
+    private JButton adelante, atras;
+    private int rango = 3;
+    private int interbalo = 1;
+    
+    private final int anchoAnuncios = 900;
+    private final int altoAnuncios = 400;
 
     // Paneles generales
     private JPanel panelNorte;
@@ -83,16 +124,46 @@ public class Ventana extends JFrame {
         setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        adelante = new JButton(new ImageIcon(getClass().getResource("Adelante.png")));
+        adelante.setBackground(Color.WHITE);
+        adelante.setBorderPainted(false);
+        adelante.setFocusPainted(false);
+        atras = new JButton(new ImageIcon(getClass().getResource("Atras.png")));
+        atras.setBackground(Color.WHITE);
+        atras.setBorderPainted(false);
+        atras.setFocusPainted(false);
+
         // Configuración del panel de productos (Sur)
         JPanel panelProductos = new JPanel();
         panelProductos.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
         panelProductos.setBackground(Color.WHITE);
 
-        panelProductos.add(producto1);
-        panelProductos.add(producto2);
-        panelProductos.add(producto3);
-        panelProductos.add(producto4);
+        panelProductos.add(atras, BorderLayout.WEST);
+        mostrar(panelProductos);
 
+        atras.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (interbalo > 1) {
+                    interbalo--;
+                } else {
+                    interbalo = 2;
+                }
+                mostrar(panelProductos);
+            }
+        });
+
+        adelante.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (interbalo < (listaProductos.length / 3)) {
+                    interbalo++;
+                } else {
+                    interbalo = 1;
+                }
+                mostrar(panelProductos);
+            }
+        });
         // Inicializar las diferentes zonas de la ventana
         initNorte();
         initCentro(); // Aquí se integran las imágenes de las ofertas
@@ -170,19 +241,31 @@ public class Ventana extends JFrame {
     private void initNorte() {
         panelNorte = new JPanel(new BorderLayout());
         panelNorte.setPreferredSize(new Dimension(700, 75));
+        panelNorte.setBackground(Color.WHITE);
 
         // Carga de imágenes para el menú superior
         JButton btnMenu = new JButton(new ImageIcon(getClass().getResource("menu.png")));
+        btnMenu.setBackground(Color.WHITE);
+        btnMenu.setBorderPainted(false);
+        btnMenu.setFocusPainted(false);
         JLabel logo = new JLabel(new ImageIcon(getClass().getResource("logo.png")));
         JButton btnCarrito = new JButton(new ImageIcon(getClass().getResource("carrito.png")));
+        btnCarrito.setBackground(Color.WHITE);
+        btnCarrito.setBorderPainted(false);
+        btnCarrito.setFocusPainted(false);
         JButton btnBuscar = new JButton(new ImageIcon(getClass().getResource("favoritos.png")));
+        btnBuscar.setBackground(Color.WHITE);
+        btnBuscar.setBorderPainted(false);
+        btnBuscar.setFocusPainted(false);
 
         JPanel panelIzquierdo = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));
         panelIzquierdo.add(btnMenu);
+        panelIzquierdo.setBackground(Color.WHITE);
 
         JPanel panelDerecha = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 5));
         panelDerecha.add(btnBuscar);
         panelDerecha.add(btnCarrito);
+        panelDerecha.setBackground(Color.WHITE);
 
         panelNorte.add(panelIzquierdo, BorderLayout.WEST);
         panelNorte.add(logo, BorderLayout.CENTER);
@@ -193,18 +276,43 @@ public class Ventana extends JFrame {
 
     private void initCentro() {
         // Aquí incorporamos el código de tu primera clase
-        JPanel panelCentro = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        JPanel panelCentro = new JPanel(new GridBagLayout());
         panelCentro.setBackground(new Color(220, 220, 220));
 
-        ImageIcon iconoTenis = new ImageIcon(getClass().getResource("Tenis.png"));
-        ImagenTeni = new JLabel(iconoTenis);
-        panelCentro.add(ImagenTeni);
+        GridBagConstraints gbc = new GridBagConstraints();
 
-        ImageIcon iconoDesc = new ImageIcon(getClass().getResource("Desc.png"));
-        Desc = new JLabel(iconoDesc);
-        panelCentro.add(Desc);
+        JLabel anuncios = new JLabel();
+        ImageIcon listaAnuncios[] = {
+            new ImageIcon(getClass().getResource("Banner (1).png")),
+            new ImageIcon(getClass().getResource("Banner (2).png")),
+            new ImageIcon(getClass().getResource("Banner (3).png")),
+            new ImageIcon(getClass().getResource("Banner (4).png")),
+            new ImageIcon(getClass().getResource("Banner (5).png"))
+        };
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        anuncios.setPreferredSize(new Dimension(anchoAnuncios, altoAnuncios));
+        anuncios.setIcon(new ImageIcon(listaAnuncios[0].getImage().getScaledInstance(anchoAnuncios, altoAnuncios, Image.SCALE_SMOOTH)));
+        panelCentro.add(anuncios, gbc);
+        Timer timer;
+        timer = new Timer(5000, new ActionListener() {
+            int i = 1;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                anuncios.setIcon(new ImageIcon(listaAnuncios[i].getImage().getScaledInstance(anchoAnuncios, altoAnuncios, Image.SCALE_SMOOTH)));
+                i++;
+                if (i >= listaAnuncios.length) {
+                    i = 0;
+                }
+            }
+        });
+        timer.start();
 
         add(panelCentro, BorderLayout.CENTER);
+
     }
 
     private void mostrarTenis(JPanel panel, ImageIcon[] listaIconos, ImageIcon[] listaColores, String desc, int prec) {
@@ -217,18 +325,18 @@ public class Ventana extends JFrame {
         JButton comprar = new JButton("Comprar");
         JButton carrito = new JButton(new ImageIcon(getClass().getResource("Carrito_blanco.png")));
         JCheckBox favs = new JCheckBox(new ImageIcon(getClass().getResource("favoritos.png")));
-        
+
         descripcion.setFont(new Font("Arial", Font.PLAIN, 16));
         descripcion.setAlignmentX(Component.LEFT_ALIGNMENT);
         costo.setFont(new Font("Arial", Font.BOLD, 18));
         costo.setAlignmentX(Component.LEFT_ALIGNMENT);
-        
+
         panelDetallesTenis.setPreferredSize(new Dimension(300, 300));
         panelDetallesTenis.setLayout(new BoxLayout(panelDetallesTenis, BoxLayout.Y_AXIS));
         panelDetallesTenis.setBackground(Color.WHITE);
-        
-        Dimension tamBotones = new Dimension(panelDetallesTenis.getPreferredSize().width,50);
-        
+
+        Dimension tamBotones = new Dimension(panelDetallesTenis.getPreferredSize().width, 50);
+
         comprar.setPreferredSize(tamBotones);
         comprar.setMaximumSize(tamBotones);
         comprar.setBackground(Color.BLACK);
@@ -239,15 +347,14 @@ public class Ventana extends JFrame {
         carrito.setBackground(Color.BLACK);
         carrito.setForeground(Color.WHITE);
         carrito.setAlignmentX(Component.LEFT_ALIGNMENT);
-        
+
         favs.setBackground(Color.WHITE);
         favs.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if(favs.isSelected()){
+                if (favs.isSelected()) {
                     favs.setIcon(new ImageIcon(getClass().getResource("favorito_Seleccionado.png")));
-                }
-                else{
+                } else {
                     favs.setIcon(new ImageIcon(getClass().getResource("favoritos.png")));
                 }
             }
@@ -280,5 +387,17 @@ public class Ventana extends JFrame {
                 }
             });
         }
+    }
+
+    private void mostrar(JPanel panelProducto) {
+        for (int i = 0; i < listaProductos.length; i++) {
+            panelProducto.add(listaProductos[i]);
+            listaProductos[i].setVisible(false);
+        }
+        for (int i = (rango * (interbalo - 1)); i < (rango * interbalo); i++) {
+            panelProducto.add(listaProductos[i]);
+            listaProductos[i].setVisible(true);
+        }
+        panelProducto.add(adelante);
     }
 }
